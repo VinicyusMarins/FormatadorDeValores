8513450f4fab3f54edcd35bd3ff4977f68482065
Vinicyus-Alves <168252000+Vinicyus-Alves@users.noreply.github.com>
2025-04-03 12:17:54 -0300

    Adicionando estilo e praticando codigo

---
+++ codigos.js
@@ -0,0 +1,5 @@
+function formatar(){ //Codigo com Erro, ainda aprendendo input
+    let format = parseFloat(document.getElementById("valor"));
+    let resultado = format * 0.85;
+    window.alert("resultado:" + resultado);
+}
