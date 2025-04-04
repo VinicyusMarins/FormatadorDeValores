0295f12348a49b0a01866738546ec069e9a5353b
Vinicyus-Alves <168252000+Vinicyus-Alves@users.noreply.github.com>
2025-04-04 12:39:12 -0300

    Código criado primeira versão

--- codigos.js
+++ codigos.js
@@ -1,5 +1,38 @@
+var resultado = 0;
+
 function formatar(){ //Codigo com Erro, ainda aprendendo input
-    let format = parseFloat(document.getElementById("valor"));
-    let resultado = format * 0.85;
-    window.alert("resultado:" + resultado);
+    let format = parseFloat(document.getElementById("valor").value);
+    if(isNaN(format)){
+        alert("Digite um valor valido!");
+        return;
+    }
+    let aprazo = format.toFixed(2).replace(".",",");
+    let avista = (format * 0.85).toFixed(2).replace(".",",");
+    
+    resultado = `${aprazo} a prazo e ${avista} a vista`;
+
+    let output = document.getElementById("resultado");
+    output.innerText = resultado;
+}
+
+function copiar(){
+    navigator.clipboard.writeText(resultado).then(() => {
+        const botao = document.getElementById("botaoCopiar");
+    
+        botao.innerText = "✔ Copiado!";
+        setTimeout(() => {
+          botao.innerText = "Copiar";
+        }, 1500);
+      });
+}
+
+document.getElementById("valor").addEventListener("keydown", function(event) {
+  if (event.key === "Enter") {
+    formatarCopiar();
+  }
+});
+
+function formatarCopiar(){
+  formatar();
+  copiar();
 }
