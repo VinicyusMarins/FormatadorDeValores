77b1ca21a6ef9d92695b83b05e04283fb8c20f77
Vinicyus-Alves <168252000+VinicyusMarins@users.noreply.github.com>
2025-07-28 19:16:30 -0300

    correção do bug NaN na formatação

--- codigos/script.js
+++ codigos/script.js
@@ -59,41 +59,84 @@ function verificarOpcao(event) {
 
 function formatarSimples(){
   let valor = parseFloat(document.getElementById("simples").value);
+  
+  if(isNaN(valor)){
+    alert('[ERRO] Insira um valor valido!');
+  }else{
+    let aprazo = valor.toFixed(2).replace(".",",");
+    avista = (valor * 0.85).toFixed(2).replace(".",",");
+      
+    resultado = `${aprazo} a prazo e ${avista} a vista`;
 
-  let aprazo = valor.toFixed(2).replace(".",",");
-  avista = (valor * 0.85).toFixed(2).replace(".",",");
-    
-  resultado = `${aprazo} a prazo e ${avista} a vista`;
-
-  output.innerText = resultado;
+    output.innerText = resultado;
+  }
 }
 
 function formatarTintas(){
   let tintaP = parseFloat(document.getElementById("tintaP").value);
   let tintaG = parseFloat(document.getElementById("tintaG").value);
 
-  let tintaPPrazo = tintaP.toFixed(2).replace(".",",")
-  let tintaGPrazo = tintaG.toFixed(2).replace(".",",");
-  let tintaPVista = (tintaP * 0.85).toFixed(2).replace(".",",")
-  let tintaGVista = (tintaG * 0.85).toFixed(2).replace(".",",");
-  resultado = `3,6L - ${tintaPPrazo} a prazo e ${tintaPVista} à vista\n18L - ${tintaGPrazo} a prazo e ${tintaGVista} à vista`;
+  if(isNaN(tintaP) && isNaN(tintaG)){
+    alert('[ERRO] Insira um valor valido!');
+  }else if(isNaN(tintaG)){
+    let tintaPPrazo = tintaP.toFixed(2).replace(".",",");
+    let tintaPVista = (tintaP * 0.85).toFixed(2).replace(".",",");
+    
+    resultado = `3,6L - ${tintaPPrazo} a prazo e ${tintaPVista} à vista`;
+    output.innerText = resultado;
+  }else if(isNaN(tintaP)){
+    let tintaGPrazo = tintaG.toFixed(2).replace(".",",");
+    let tintaGVista = (tintaG * 0.85).toFixed(2).replace(".",",");
+    
+    resultado = `18L - ${tintaGPrazo} a prazo e ${tintaGVista} à vista`;
 
-  output.innerText = resultado;
+    output.innerText = resultado;
+  }else{
+    let tintaPPrazo = tintaP.toFixed(2).replace(".",",");
+    let tintaPVista = (tintaP * 0.85).toFixed(2).replace(".",",");
+
+    let tintaGPrazo = tintaG.toFixed(2).replace(".",",");
+    let tintaGVista = (tintaG * 0.85).toFixed(2).replace(".",",");
+
+    resultado = `3,6L - ${tintaPPrazo} a prazo e ${tintaPVista} à vista\n18L - ${tintaGPrazo} a prazo e ${tintaGVista} à vista`;
+
+    output.innerText = resultado;
+  }
 }
 
 function formatarMassas(){
-  let massaP = parseFloat(document.getElementById("massaP").value)
-  let massaG = parseFloat(document.getElementById("massaG").value)
+  let massaP = parseFloat(document.getElementById("massaP").value);
+  let massaG = parseFloat(document.getElementById("massaG").value);
 
-  let massaPPrazo = massaP.toFixed(2).replace(".",",")
-  let massaGPrazo = massaG.toFixed(2).replace(".",",")
+  if(isNaN(massaP) && isNaN(massaG)){
+    alert('[ERRO] Insira um valor valido!');
 
-  let massaPVista = (massaP * 0.85).toFixed(2).replace(".",",")
-  let massaGVista = (massaG * 0.85).toFixed(2).replace(".",",")
+  }else if(isNaN(massaG)){
+    let massaPPrazo = massaP.toFixed(2).replace(".",",");
+    let massaPVista = (massaP * 0.85).toFixed(2).replace(".",",");
+    
+    resultado = `3,6L - ${massaPPrazo} a prazo e ${massaPVista} à vista`;
+    output.innerText = resultado;
 
-  resultado = `5,8kg - ${massaPPrazo} a prazo e ${massaPVista} à vista\n25kg - ${massaGPrazo} a prazo e ${massaGVista} à vista`;
+  }else if(isNaN(massaP)){
+    let massaGPrazo = massaG.toFixed(2).replace(".",",");
+    let massaGVista = (massaG * 0.85).toFixed(2).replace(".",",");
+    
+    resultado = `18L - ${massaGPrazo} a prazo e ${massaGVista} à vista`;
 
-  output.innerText = resultado;
+    output.innerText = resultado;
+    
+  }else{
+    let massaPPrazo = massaP.toFixed(2).replace(".",",");
+    let massaPVista = (massaP * 0.85).toFixed(2).replace(".",",");
+
+    let massaGPrazo = massaG.toFixed(2).replace(".",",");
+    let massaGVista = (massaG * 0.85).toFixed(2).replace(".",",");
+
+    resultado = `3,6L - ${massaPPrazo} a prazo e ${massaPVista} à vista\n18L - ${massaGPrazo} a prazo e ${massaGVista} à vista`;
+
+    output.innerText = resultado;
+  }
 }
 
 function copiarSimples(){
@@ -160,10 +203,19 @@ function formatarEnter(event, input) {
       executarSimples();
       document.querySelector('input#simples').value = ''
       break;
+    case "tintaP":
+      executarTintas();
+      document.querySelector('input#tintaP').value = ''
+      break;
     case "tintaG":
       executarTintas();
       document.querySelector('input#tintaP').value = ''
       document.querySelector('input#tintaG').value = ''
+      break;
+
+    case "massaP":
+      executarMassas();
+      document.querySelector('input#massaP').value = ''
       break;
     case "massaG":
       executarMassas();
